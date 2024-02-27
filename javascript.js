085a9aeffa9b58f6ce15382034a49ba3b1b3d49a
Max Gagner <m.gagner@hotmail.se>
2024-02-27 22:24:11 +0100

    added css and responsiveness to charts

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 import Chart from 'chart.js/auto'
+let color;
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
     document.getElementsByTagName("html")[0].setAttribute("class", "dark");
+    color = false;
 }
 
 //inital call to api and conversion to a map for use in all subfunctions (same as L2 but changed what data to get)
@@ -24,6 +26,13 @@ async function getData() {
 
 //setup for 
 function start(courseMap) {
+
+    //makes it readable if you use darkmode
+    if(!color) {
+        Chart.defaults.color = "#ffffff";
+    }
+
+    //if page diagram
     if(document.getElementById("bar")) {
         courseMap.sort((a, b) => b[1] - a[1]);
         let smallMap = courseMap.slice(0, 6);
@@ -39,22 +48,12 @@ function start(courseMap) {
                     data: values
                 }]
             },
+            //makes the chart more responsive
             options: {
-                plugins: {
-                    legend: {
-                        labels: {
-                            font: {
-                                size: 40
-                            }
-                        }
-                    }
-                },
-                onHover: {
-                   
-                }
+                responsive: true,
+                maintainAspectRatio: false
             }
         });
-
         names.pop();
         values.pop();
         new Chart(circle, {
@@ -69,10 +68,13 @@ function start(courseMap) {
         });
         
     }
+
+    //if page map
     else if(document.getElementById("map")) {
 
     }
 }
+
 
 //startup
 getData()

5532f692826b99ab3c25f42a8ae7c9fe966c8743
Max Gagner <m.gagner@hotmail.se>
2024-02-28 02:06:53 +0100

    fixed a working map!

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 import Chart from 'chart.js/auto'
 let color = true;
+let map;
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
     document.getElementsByTagName("html")[0].setAttribute("class", "dark");
@@ -14,7 +15,7 @@ async function getData() {
             console.log("problem with fetch content")
         }
         let data = await rawData.json();
-        courseMap = data.map(function(data) {
+        let courseMap = data.map(function(data) {
             return [data.name, data.applicantsTotal];
         });
         start(courseMap);
@@ -40,11 +41,14 @@ function start(courseMap) {
 
     //if page diagram
     if(document.getElementById("bar")) {
+
+        //sort and then take 6 largest by value
         courseMap.sort((a, b) => b[1] - a[1]);
         let smallMap = courseMap.slice(0, 6);
         let names = smallMap.map(val => val[0]);
         let values = smallMap.map(val => val[1]);
         
+        //blockchart
         new Chart(bar, {
             type: "bar",
             data: {
@@ -67,8 +71,12 @@ function start(courseMap) {
                 }
             }
         });
+
+        //decrease size to 5
         names.pop();
         values.pop();
+
+        //piechart
         new Chart(circle, {
             type: "pie",
             data: {
@@ -84,10 +92,48 @@ function start(courseMap) {
 
     //if page map
     else if(document.getElementById("map")) {
-
+        maps();
     }
 }
 
+//using leaflet for map
+function maps() {
+    //inital code mostly from leaflet (https://leafletjs.com/examples/quick-start/) values customized
+    map = L.map('map').setView([62.393713, 17.283529], 17);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+}
+
+//search
+window.search = function search(val) {
+    fetchSearch("https://nominatim.openstreetmap.org/search?format=json&q=" + val)
+}
+
+function move(location) {
+    location = location[0];
+    console.log(location)
+    map.flyTo([location[1], location[2]], 8);
+}
+
+//a bit messy but dident wanna restructure
+async function fetchSearch(url) {  
+    try {
+        let rawData = await fetch(url);
+        if(!rawData.ok) {
+            console.log("problem with fetch content")
+        }
+        let data = await rawData.json();
+        let map = data.map(function(data) {
+            return [data.display_name, data.lat, data.lon];
+        });
+        move(map);
+    }
+    catch(error) {
+        console.log("it broke" + error)
+    }
+}
 
 //startup
 getData()
